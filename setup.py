ddcb2ea0562a69f884ba033324a4a4dbe3950b36
hexoul <crosien@gmail.com>
2021-06-16 09:54:45 +0900

    Address partial comments of @minwoox

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python
+import os
 from setuptools import setup
 
-
+package_root = os.path.abspath(os.path.dirname(__file__))
+readme_filename = os.path.join(package_root, "README.md")
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -14,16 +15,19 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/line/centraldogma-python",
     author="",
-    license="MIT",
     author_email="",
+    license="Apache 2.0",
     install_requires=["requests", "marshmallow", "dataclasses-json"],
+    python_requires=">=3.7",
     keywords="centraldogma",
     classifiers=[
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
