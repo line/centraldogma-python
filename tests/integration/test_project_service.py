46234ed1501a5b2d4c308f8f0264691278f2dc44
hexoul <crosien@gmail.com>
2021-09-06 01:42:21 +0900

    Add integration tests for RepositoryService

--- tests/integration/test_project_service.py
+++ tests/integration/test_project_service.py
@@ -17,6 +17,7 @@ import pytest
 import os
 
 dogma = Dogma()
+project_name = "TestProject"
 
 
 @pytest.mark.skipif(
@@ -25,24 +26,32 @@ dogma = Dogma()
 )
 @pytest.mark.integration
 def test_project():
-    projects = dogma.list_projects()
-    assert len(projects) == 0
-
     with pytest.raises(BadRequestException):
         dogma.create_project("Test project")
 
-    project_name = "TestProject"
+    len_project = len(dogma.list_projects())
+    len_removed_project = len(dogma.list_projects(removed=True))
+
     new_project = dogma.create_project(project_name)
     assert new_project.name == project_name
+    validate_len(len_project + 1, len_removed_project)
 
-    projects = dogma.list_projects()
-    assert len(projects) == 1
-
-    removed = dogma.remove_project("TestProject")
+    removed = dogma.remove_project(project_name)
     assert removed == True
+    validate_len(len_project, len_removed_project + 1)
 
-    projects = dogma.list_projects(removed=True)
-    assert len(projects) == 1
+    unremoved = dogma.unremove_project(project_name)
+    assert unremoved.name == project_name
+    validate_len(len_project + 1, len_removed_project)
 
+    dogma.remove_project(project_name)
+    purged = dogma.purge_project(project_name)
+    assert purged == True
+    validate_len(len_project, len_removed_project)
+
+
+def validate_len(expected_len, expected_removed_len):
     projects = dogma.list_projects()
-    assert len(projects) == 0
+    removed_projects = dogma.list_projects(removed=True)
+    assert len(projects) == expected_len
+    assert len(removed_projects) == expected_removed_len
---
+++ tests/integration/test_repository_service.py
@@ -0,0 +1,63 @@
+# Copyright 2021 LINE Corporation
+#
+# LINE Corporation licenses this file to you under the Apache License,
+# version 2.0 (the "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at:
+#
+#   https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from centraldogma.dogma import Dogma
+from centraldogma.exceptions import BadRequestException
+import pytest
+import os
+
+dogma = Dogma()
+project_name = "TestProject"
+repo_name = "TestRepository"
+
+
+@pytest.fixture(scope="module")
+def run_around_test():
+    dogma.create_project(project_name)
+    yield
+    dogma.remove_project(project_name)
+    dogma.purge_project(project_name)
+
+
+@pytest.mark.skipif(
+    os.getenv("INTEGRATION_TEST", "false").lower() != "true",
+    reason="Integration tests are disabled. Use `INTEGRATION_TEST=true pytest` to enable them.",
+)
+@pytest.mark.integration
+def test_repository(run_around_test):
+    len_repo = len(dogma.list_repositories(project_name))
+    len_removed_repo = len(dogma.list_repositories(project_name, removed=True))
+
+    repo = dogma.create_repository(project_name, repo_name)
+    assert repo.name == repo_name
+    validate_len(len_repo + 1, len_removed_repo)
+
+    removed = dogma.remove_repository(project_name, repo_name)
+    assert removed == True
+    validate_len(len_repo, len_removed_repo + 1)
+
+    unremoved = dogma.unremove_repository(project_name, repo_name)
+    assert unremoved.name == repo_name
+    validate_len(len_repo + 1, len_removed_repo)
+
+    dogma.remove_repository(project_name, repo_name)
+    purged = dogma.purge_repository(project_name, repo_name)
+    assert purged == True
+    validate_len(len_repo, len_removed_repo)
+
+
+def validate_len(expected_len, expected_removed_len):
+    repos = dogma.list_repositories(project_name)
+    removed_repos = dogma.list_repositories(project_name, removed=True)
+    assert len(repos) == expected_len
+    assert len(removed_repos) == expected_removed_len
